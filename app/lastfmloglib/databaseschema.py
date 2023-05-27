f091c178cb4d6c9ef1da6633b149c41572196e30
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-05-27 15:44:59 +0200

    Janitor

--- app/lastfmloglib/databaseschema.py
+++ app/lastfmloglib/databaseschema.py
@@ -1,20 +1,20 @@
 # Database schema (transaction)
 databaseSchema = '''
-    BEGIN;
+BEGIN;
 
-    CREATE TABLE IF NOT EXISTS trackslog (
-        playHash TEXT NOT NULL UNIQUE,
-        playTime INTEGER NOT NULL UNIQUE,
-        artist TEXT NOT NULL,
-        track TEXT NOT NULL,
-        album TEXT DEFAULT NULL,
-        PRIMARY KEY(playHash)
-    );
+CREATE TABLE IF NOT EXISTS trackslog (
+    playHash TEXT NOT NULL UNIQUE,
+    playTime INTEGER NOT NULL UNIQUE,
+    artist TEXT NOT NULL,
+    track TEXT NOT NULL,
+    album TEXT DEFAULT NULL,
+    PRIMARY KEY(playHash)
+);
 
-    CREATE INDEX indexPlayTime ON trackslog(playTime DESC);
-    CREATE INDEX indexArtist ON trackslog(artist COLLATE NOCASE ASC);
-    CREATE INDEX indexTrack ON trackslog(track COLLATE NOCASE ASC);
-    CREATE INDEX indexAlbum ON trackslog(album COLLATE NOCASE ASC);
+CREATE INDEX indexPlayTime ON trackslog(playTime DESC);
+CREATE INDEX indexArtist ON trackslog(artist COLLATE NOCASE ASC);
+CREATE INDEX indexTrack ON trackslog(track COLLATE NOCASE ASC);
+CREATE INDEX indexAlbum ON trackslog(album COLLATE NOCASE ASC);
 
-    COMMIT;
+COMMIT;
 '''
