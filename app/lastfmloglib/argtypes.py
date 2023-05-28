0b7ad0bbad68df93dcca4b0c4d8a66e13280b6f6
arT2 <60965610+etrusci-org@users.noreply.github.com>
2023-05-28 13:04:20 +0200

    Enhanced arg type/value-check functions

--- app/lastfmloglib/argtypes.py
+++ app/lastfmloglib/argtypes.py
@@ -7,27 +7,42 @@ import time
 
 def cliparserArgTypeQueryLimit(number: int) -> int:
     number = int(number)
-    if number < 1:
+
+    if number <= 0:
         raise argparse.ArgumentTypeError('must be > 0')
+
     return number
 
 
 def cliparserArgTypeFrom(number: int) -> int:
     number = int(number)
+
     if number < 0:
         raise argparse.ArgumentTypeError('must be >= 0')
+
+    if number >= int(time.time()):
+        raise argparse.ArgumentTypeError('must be < unixtime_now')
+
     return number
 
 
 def cliparserArgTypeTo(number: int) -> int:
     number = int(number)
-    if number > time.time():
-        raise argparse.ArgumentTypeError('can not be in the future')
+
+    if number <= 0:
+        raise argparse.ArgumentTypeError('must be > 0')
+
+    if number > int(time.time()):
+        raise argparse.ArgumentTypeError('must be <= unixtime_now')
+
     return number
 
 
 def cliparserArgTypeExistingDirectoryPath(dirPath: str) -> str:
-    if not os.path.exists(dirPath) \
-    or not os.path.isdir(dirPath):
-        raise argparse.ArgumentTypeError('directory does not exist or path does not point to a direcotry')
+    if not os.path.exists(dirPath):
+        raise argparse.ArgumentTypeError('path does not exist')
+
+    if not os.path.isdir(dirPath):
+        raise argparse.ArgumentTypeError('path does not point to a directory')
+
     return dirPath
